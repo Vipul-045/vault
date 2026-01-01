3de5ab62217a1be561334a9c75e80aa4d66c1061
Vipul-045 <vipulyadav0479@gmail.com>
2026-01-01 15:04:19 +0530

    InitializeVault instruction

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -6,11 +6,11 @@ declare_id!("69TavcFc8E4cjPi5z7kds5YzP4UZJjFCxKX32b1nY8m4");
 pub mod vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeVault>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeVault {}
